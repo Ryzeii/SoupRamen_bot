20168ddb507622400b5ac392125034f36fd54f2f
Ryzeii <antypovanton93@gmail.com>
2025-03-20 17:04:09 +0500

    init

---
+++ src/components/hooks/useTelegram.js
@@ -0,0 +1,24 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram() {
+
+    const onClose = ()=>{
+        tg.close();
+    }
+    const onToggleButton = ()=>{
+        if(tg.MainButton.isViseble){
+            tg.MainButton.hide();
+        }
+        else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+
+        tg,
+        user:tg.initDataUnsafe?.user?.username,
+        onClose,
+        onToggleButton,
+    }
+}
